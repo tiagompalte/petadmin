737a863d365f73f9b9cdcbc7e4f058aa8c02f195
Tiago Mariani Palte <tiagompalte@hotmail.com>
2019-12-28 16:35:00 -0300

    atualizando a campanha

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,8 +7,16 @@ class Campaign < ApplicationRecord
   has_many :campaign_clients, dependent: :destroy
   has_many :clients, through: :campaign_clients
 
+  after_save :schedule_emails
+
   def fae_display_field
     title
   end
 
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
 end
