aec99c3b56a297431c4575b8934e1da0be0a849e
Tiago Mariani Palte <tiagompalte@hotmail.com>
2019-12-28 10:23:53 -0300

    setup da gem fae

---
+++ config/initializers/fae.rb
@@ -0,0 +1,66 @@
+Fae.setup do |config|
+
+  config.devise_secret_key = 'f497245b2b016ba8e20898604b8f4a720de38c13f9129cae51fec8e246c203dd87748f2ddbd0be70accfc199fa59da54d47718566014f9643316c9f8f4aea069'
+
+
+  ## devise_mailer_sender
+  # This email address will get passed to Devise and
+  # used as the from address in the password reset emails.
+  # config.devise_mailer_sender = 'change-me@example.com'
+
+  ## dashboard_exclusions
+  # The dashboard will show all objects with recent activity.
+  # To exclude any objects, add them to this array.
+  # config.dashboard_exclusions = %w( Gallery )
+
+  ## max_image_upload_size
+  # This will set a file size limit on image uploads in MBs.
+  # The default is 2 MB.
+  # config.max_image_upload_size = 2
+
+  ## max_file_upload_size
+  # This will set a file size limit on file uploads in MBs.
+  # The default is 5 MB.
+  # config.max_file_upload_size = 5
+
+  ## languages
+  # This hash sets the supported languages for the multiple
+  # language toggle feature.
+  # config.languages = {
+  #   en: 'English',
+  #   zh: 'Chinese'
+  # }
+
+  ## recreate_versions
+  # Triggers `Fae::Image` to recreate Carrierwave versions after save.
+  # Defaults to false
+  # config.recreate_versions = false
+
+  ## track_changes
+  # This is the global toggle for the change tracker.
+  # Defaults to true
+  # config.track_changes = true
+
+  ## tracker_history_length
+  # This defines how many changes per object are kept in the DB
+  # via the change tracker.
+  # Defaults to 15
+  # config.tracker_history_length = 15
+
+  ## disabled_environments
+  # This option will disable Fae complete when the app is running
+  # on one of the defined environments
+  # config.disabled_environments = [ :preview, :staging ]
+
+  ## per_page
+  # Sets the default number of items shown in paginated lists
+  # Defaults to 25
+  # config.per_page = 25
+
+  ## use_cache
+  # Determines whether or not Fae will utilize cache internally.
+  # Note: you still need to enable `perform_caching` and set a `cache_store`
+  # on the parent app
+  # Defaults to false
+  # config.use_cache = true
+end
